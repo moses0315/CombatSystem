9eb8716a9ac6ab8a5fe0f864cc70c3c952e91f59
Notain <bitnari707070@gmail.com>
2023-09-23 21:38:54 +0900

    enemy attack settup

--- Combat/enemy.gd
+++ Combat/enemy.gd
@@ -4,19 +4,66 @@ const SPEED = 100
 var player_chase = false
 var player = null
 
+var health = 20
+var attack_power = 10
+var is_hurt = false
+var is_attacking = false
+
 @onready var anim = $AnimationPlayer
+@onready var sprite = $AnimatedSprite2D
+
+@onready var healthbar = $Healthbar
+
+@onready var attack_cooldown_timer = $AttackCooldownTimer
 
 func _physics_process(delta):
-	if player_chase:
+	#Health Bar
+	healthbar.value = health
+		
+	#Move
+	if player_chase and not is_hurt and not is_attacking:
 		var direction = (player.position - position).normalized()
 		if direction.x > 0:
 			$AnimatedSprite2D.flip_h = false
 		else:
 			$AnimatedSprite2D.flip_h = true
 		velocity.x = direction.x * SPEED
+		attack()
+	elif is_attacking or is_hurt:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	move_and_slide()
 
 func _on_player_detection_area_body_entered(body):
 	player = body
 	player_chase = true
 
+func attack():
+	if Input.is_action_just_pressed("attack"):#-> 캐릭터가 어택디텍트 에어리아 들어오면 
+		is_attacking = true
+		attack_cooldown_timer.start()
+		if sprite.flip_h == true:
+			anim.play("attack1")
+		else:
+			anim.play("attack1")
+			
+func take_damage(attack_power):
+	health -= attack_power
+	is_hurt = true
+	if health <= 0:
+		anim.play("death")
+		await anim.animation_finished
+		self.queue_free()
+	else:
+		anim.play("hit")
+
+
+func _on_animation_player_animation_finished(anim_name = "hit"):
+	is_hurt = false
+	anim.play("idle")
+	
+func _on_attack_detection_area_body_entered(body):
+	body.take_damage(attack_power)
+
+
+func _on_attack_cooldown_timer_timeout():
+	is_attacking = false
--- Combat/player.gd
+++ Combat/player.gd
@@ -6,12 +6,28 @@ const BACKSTEP_SPEED = 300
 var is_backsteping = false
 var is_backstep_cooldown = false
 
+var is_attacking = false
+var is_hurt = false
+
+var health = 100
+var attack_power = 10
+
 @onready var anim = $AnimationPlayer
 @onready var sprite = $AnimatedSprite2D
+
 @onready var backstep_timer = $BackstepTimer
 @onready var backstep_cooldown_timer = $BackstepCooldownTimer
+@onready var attack_cooldown_timer = $AttackCooldownTimer
 
+@onready var healthbar = $Healthbar
+
+func _ready():
+	$AttackArea/CollisionShape2D.disabled = true
+	
 func _physics_process(delta):
+	#Health Bar
+	healthbar.value = health
+	
 	#Player Move
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction == -1:
@@ -19,7 +35,7 @@ func _physics_process(delta):
 	elif direction == 1:
 		sprite.flip_h = false	
 
-	if not is_backsteping:
+	if not is_backsteping and not is_attacking and not is_hurt:
 		if direction:
 			velocity.x = direction * MOVE_SPEED
 			anim.play("run")
@@ -37,17 +53,43 @@ func _physics_process(delta):
 		else:
 			velocity.x = move_toward(velocity.x, 0, MOVE_SPEED)
 			anim.play("idle")
-			
+		attack()	
+	elif is_attacking or is_hurt:
+		velocity.x = move_toward(velocity.x, 0, MOVE_SPEED)
+		
 	move_and_slide()
 	
+func attack():
+	if Input.is_action_just_pressed("attack"):
+		is_attacking = true
+		attack_cooldown_timer.start()
+		if sprite.flip_h == true:
+			anim.play("attack1")
+		else:
+			anim.play("attack2")
+
+func take_damage(attack_power):
+	health -= attack_power
+	is_hurt = true
+	if health <= 0:
+		anim.play("death")
+		await anim.animation_finished
+	else:
+		anim.play("hit")
+		
+		
 func _on_backstep_cooldown_timer_timeout():
 	is_backstep_cooldown = false
 				
 func _on_backstep_timer_timeout():
 	is_backsteping = false
 
-
+func _on_attack_cooldown_timer_timeout():
+	is_attacking = false
 
 func _on_attack_area_body_entered(body):
-	#body.take_damage()
-	print(123)
+	body.take_damage(attack_power)
+
+func _on_animation_player_animation_finished(anim_name = "hit"):
+	is_hurt = false
+	anim.play("idle")
